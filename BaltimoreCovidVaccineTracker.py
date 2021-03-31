1a0eaa23324e6914711ca2228b4a91449c16907a
Charlton Flanders <flander.chad@gmail.com>
2021-03-30 21:45:38 -0400

    Splitting out scraper

--- BaltimoreCovidVaccineTracker.py
+++ BaltimoreCovidVaccineTracker.py
@@ -1,42 +1,10 @@
-import sys
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 from matplotlib import animation
 from itertools import combinations
 
-def scrape():
-	URL = 'https://coronavirusvaccineoutreach-bc-gis.hub.arcgis.com/#statistics'
-
-	print("Opening browser")
-	fireFoxOptions = webdriver.FirefoxOptions()
-	fireFoxOptions.headless = True
-	driver = webdriver.Firefox(options=fireFoxOptions)
-
-	print("Navigating to webpage")
-	driver.get(URL)
-	#print(driver.page_source)
-
-	print("Waiting for js to load (20 second timeout)")
-	try: 
-		element = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "ss-value"))) 
-	except:
-		print("JS Didn't Load")
-		
-	numFirstStr = driver.find_element_by_xpath("/html/body/div[6]/div[2]/div/div[1]/div[3]/div/div/section[3]/div/div[2]/div[2]/div/div[2]/span[1]").text
-	numSecondStr = driver.find_element_by_xpath("/html/body/div[6]/div[2]/div/div[1]/div[3]/div/div/section[3]/div/div[4]/div[2]/div/div[2]/span[1]").text
-	percentFirstStr = driver.find_element_by_xpath("/html/body/div[6]/div[2]/div/div[1]/div[3]/div/div/section[3]/div/div[3]/div/div/div/div").text
-	percentSecondStr = driver.find_element_by_xpath("/html/body/div[6]/div[2]/div/div[1]/div[3]/div/div/section[3]/div/div[5]/div/div/div/div").text
-
-	print(percentFirstStr + " have had their first shot.")
-	print(percentSecondStr + " have had their second shot.")
-
-	return float(percentFirstStr.replace('%','')), float(percentSecondStr.replace('%','')) 
+import BCVTScraper
 
 class Particle:
 	"""A class representing a two-dimensional particle."""
@@ -314,7 +282,7 @@ if __name__ == '__main__':
 	nparticles = 110
 	radii = 0.008
 
-	percentFirstShot, percentSecondShot = scrape();
+	percentFirstShot, percentSecondShot = BCVTScraper.scrape();
 
 	percentGreen = percentSecondShot
 	percentYellow = percentFirstShot - percentSecondShot
